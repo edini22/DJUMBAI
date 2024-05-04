98ecbe03ec1bd82209bdc1f46565d2ce201d9bac
edini22 <figueiredo92002@gmail.com>
2024-05-04 14:34:52 +0100

    v1.1

--- bino/djumbai-inject.cpp
+++ bino/djumbai-inject.cpp
@@ -1,45 +1,47 @@
 
-#include <iostream>
-#include <unistd.h>
-#include <fcntl.h>
-#include <sys/wait.h>
 #include <cstring>
+#include <fcntl.h>
+#include <iostream>
+#include <sys/wait.h>
+#include <unistd.h>
 
-int main()
-{
+using namespace std;
+
+struct Message {
+    string sender;
+    string receiver;
+    string subject;
+};
+
+int main() {
     int input_pipe[2];
     int output_pipe[2];
     // Create pipes for input and output
-    if (pipe(input_pipe) == -1 || pipe(output_pipe) == -1)
-    {
-        std::cerr << "Failed to create pipes\n";
+    if (pipe(input_pipe) == -1 || pipe(output_pipe) == -1) {
+        cerr << "Failed to create pipes\n";
         return 1;
     }
 
     pid_t pid = fork();
 
-    if (pid == -1)
-    {
-        std::cerr << "Failed to fork\n";
+    if (pid == -1) {
+        cerr << "Failed to fork\n";
         return 1;
     }
 
-    if (pid == 0)
-    { // Child process
+    if (pid == 0) { // Child process
         // Close unused ends of the pipes
         close(input_pipe[1]);  // Close the write end of the input pipe
         close(output_pipe[0]); // Close the read end of the output pipe
 
         // Replace standard input and output with pipe file descriptors
-        if (dup2(input_pipe[0], STDIN_FILENO) == -1)
-        {
-            std::cerr << "Failed to duplicate input file descriptor\n";
+        if (dup2(input_pipe[0], STDIN_FILENO) == -1) {
+            cerr << "Failed to duplicate input file descriptor\n";
             return 1;
         }
 
-        if (dup2(output_pipe[1], STDOUT_FILENO) == -1)
-        {
-            std::cerr << "Failed to duplicate output file descriptor\n";
+        if (dup2(output_pipe[1], STDOUT_FILENO) == -1) {
+            cerr << "Failed to duplicate output file descriptor\n";
             return 1;
         }
 
@@ -51,30 +53,49 @@ int main()
         execl("./djumbai-queue", "djumbai-queue", NULL);
 
         // If execl fails, it means the program failed to execute
-        std::cerr << "Failed to execute the program\n";
+        cerr << "Failed to execute the program\n";
         return 1;
-    }
-    else
-    { // Parent process
+    } else { // Parent process
         // Close unused ends of the pipes
         close(input_pipe[0]);  // Close the read end of the input pipe
         close(output_pipe[1]); // Close the write end of the output pipe
 
         // Write strings to input_pipe[1]
-        const char *string1 = "Hello";
-        const char *string2 = "World";
-        write(input_pipe[1], string1, strlen(string1));
-        write(input_pipe[1], string2, strlen(string2));
+
+        cout << "Enter a message: " << endl;
+        // string message;
+        // cin >> message;
+
+        char word;
+        string message;
+        while (cin.get(word)) {
+            message += word;
+        }
+
+        cout << "input foi: " << message << endl;
+
+        if (message.length() <= 0 || message.length() > 512) {
+            cerr << "Input must have size between 0 and 512";
+        }
+
+        string message_tosend = "M";
+        message_tosend.append(message);
+        message_tosend.append("\n!<MESSAGE>!");
+
+        cout << "Depois do apppend" << message_tosend << endl;
+        const char *message_buffer = message_tosend.c_str();
+        cout << "Sending to child: " << message_buffer << endl;
+        write(input_pipe[1], message_buffer, strlen(message_buffer));
+
         close(input_pipe[1]); // Close the write end of the input pipe
 
         // Read strings from output_pipe[0]
         char buffer[256];
         ssize_t bytesRead;
-        while ((bytesRead = read(output_pipe[0], buffer, sizeof(buffer))) > 0)
-        {
+        while ((bytesRead = read(output_pipe[0], buffer, sizeof(buffer))) > 0) {
             // Null-terminate the buffer to print it as a string
             buffer[bytesRead] = '\0';
-            std::cout << "Received from child: " << buffer;
+            cout << "Received from child: " << buffer;
         }
         close(output_pipe[0]); // Close the read end of the output pipe
 
--- bino/djumbai-queue.cpp
+++ bino/djumbai-queue.cpp
@@ -1,29 +1,77 @@
+#include <cstdio>
+#include <filesystem>
+#include <fstream>
 #include <iostream>
 #include <string>
-#include <fstream>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
 
-int main()
-{
-    std::string input1, input2;
+using namespace std;
+namespace fs = std::filesystem;
 
-    // Lê as duas strings da entrada padrão
-    std::cin >> input1 >> input2;
+int main() {
+    string input1;
+
+    string message;
+    getchar();
+    int count = 0;
+    while (getline(std::cin, input1) && !input1.empty()) {
+        if (input1 == "!<MESSAGE>!") {
+            break;
+        }
+        count += input1.length();
+        if (count > 512) {
+            cerr << "Mensagem foi forjada" << endl;
+            exit(1);
+        }
+        message += input1 + "\n";
+        // cout << "x: " << input1 << endl;
+    }
 
     // Exibe as strings recebidas
-    std::cout << "String 1: " << input1 << std::endl;
-    std::cout << "String 2: " << input2 << std::endl;
+    cout << "Message: " << message << endl;
+    // cout << "String 2: " << input2 << endl;
 
+    pid_t pid = getpid();
 
-    std::ofstream file("../queue/mess/message.txt"); //#TODO: mudar nome do arquivo e permissoes
-    if (!file.is_open())
-    {
-        std::cerr << "Failed to open file for writing.\n";
+    const string path = "../queue/pid/" + to_string(pid) + ".mdjumbai";
+    const char *pid_filename = path.c_str();
+
+    ofstream file(path); // #TODO: mudar nome do arquivo e permissoes
+    if (!file.is_open()) {
+        cerr << "Failed to open file for writing.\n";
         return 1;
     }
 
     // Write the message to the file
-    file << input1;
+    file << message;
     file.close();
+
+    struct stat fileStat;
+
+    // Usando lstat para obter informações sobre o arquivo, incluindo o inode
+    if (lstat(pid_filename, &fileStat) == -1) {
+        std::cerr << "Erro ao obter informações sobre o arquivo.\n";
+        return 1;
+    }
+    // Exibir o número do inode
+    cout << "Número do inode de " << pid_filename << ": " << fileStat.st_ino << endl;
+
+    const string dest_filename = "../queue/mess/" + to_string(fileStat.st_ino) + ".mdjumbai";
+    const char *dest_dest_filename = dest_filename.c_str();
+    // move file to mess folder
+
+    // fs::rename(path, dest_filename);
+    // fs::remove(path);
+    int resultado = rename(pid_filename, dest_dest_filename);
+
+    if (resultado == 0) {
+        printf("Arquivo renomeado com sucesso.\n");
+    } else {
+        perror("Erro ao renomear o arquivo");
+        return 1;
+    }
 
     return 0;
 }
